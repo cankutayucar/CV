e399010b1159ac74ac108e3d970a2854b1eae93b
cankutay <cankutayucar@hotmail.com>
2022-07-08 15:02:45 +0300

    degistirme

--- CankutayUcarCV.Web/Helper/Mail/SmtpSettings.cs
+++ CankutayUcarCV.Web/Helper/Mail/SmtpSettings.cs
@@ -5,8 +5,8 @@
         public string Server { get; set; } = "smtp.office365.com";
         public int Port { get; set; } = 587;
         public string GondericiAdi { get; set; } = "kişi";
-        public string GondericiEmailAdres { get; set; } = "cankutayucar@hotmail.com";
-        public string KullaniciAdi { get; set; } = "cankutayucar@hotmail.com";
-        public string Sifre { get; set; } = "456987";
+        public string GondericiEmailAdres { get; set; } = "";
+        public string KullaniciAdi { get; set; } = "";
+        public string Sifre { get; set; } = "";
     }
 }
